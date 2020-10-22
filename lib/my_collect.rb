0a95ecdc6b5febc0637726e9bdb5b30891120d96
jessijoke <jessijoke@yahoo.com>
2020-10-21 22:04:05 -0400

    commit tree e380b77621eb7ecac0877d9cf5c9884044da1dfd

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
-
+def my_collect(arr)
+    i = 0
+    newarr = []
+    while i < arr.length
+        puts arr[i]
+        current = yield(arr[i])
+        newarr << current
+        i += 1
+    end
+    puts "#{newarr} new arr"
+    newarr
+end
